99b342fec368eac0f2ba129f678696298304f4ea
jash0622 <jash22bh@gmail.com>
2024-10-26 16:32:23 +0530

    Main Dashboard

--- Dasboard-UI/vite.config.js
+++ Dasboard-UI/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Relative path for serving the app
   build: {
     outDir: 'dist'
   }
